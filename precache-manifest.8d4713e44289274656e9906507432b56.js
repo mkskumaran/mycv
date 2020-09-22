15e57ea06f26aa59a98a36f1ba607c5b8289187d
ShunmugaKumaran M <34643845+mkskumaran@users.noreply.github.com>
2020-09-22 11:45:40 +0530

    Updates

---
+++ precache-manifest.8d4713e44289274656e9906507432b56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cf13a5e6ae8ac72f13e84dfe2b304c",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "464def0e490872809a30",
+    "url": "/mycv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "195ba381ae080bc9af5b",
+    "url": "/mycv/static/js/2.2a6cdf61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mycv/static/js/2.2a6cdf61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464def0e490872809a30",
+    "url": "/mycv/static/js/main.464e11c7.chunk.js"
+  },
+  {
+    "revision": "9c9156fde34d30785013",
+    "url": "/mycv/static/js/runtime-main.f7b5f11b.js"
+  }
+]);
