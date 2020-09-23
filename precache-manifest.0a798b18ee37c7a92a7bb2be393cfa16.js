8392e97d34b8bfee9791611222491ce9a5b2b48c
ShunmugaKumaran M <34643845+mkskumaran@users.noreply.github.com>
2020-09-23 16:56:35 +0530

    Updates

---
+++ precache-manifest.0a798b18ee37c7a92a7bb2be393cfa16.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c333eceed6dc4faa2d8e1cab92449e5",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "5fd38abba8148b0effc9",
+    "url": "/mycv/static/css/2.a286b187.chunk.css"
+  },
+  {
+    "revision": "02c9e66659bffdfd1ea4",
+    "url": "/mycv/static/css/main.bbb1bf6a.chunk.css"
+  },
+  {
+    "revision": "5fd38abba8148b0effc9",
+    "url": "/mycv/static/js/2.d540b46a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mycv/static/js/2.d540b46a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c9e66659bffdfd1ea4",
+    "url": "/mycv/static/js/main.325a2403.chunk.js"
+  },
+  {
+    "revision": "9c9156fde34d30785013",
+    "url": "/mycv/static/js/runtime-main.f7b5f11b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mycv/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mycv/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mycv/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mycv/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mycv/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "141855a9d5d56aa35801ae6837313f36",
+    "url": "/mycv/static/media/launch_as_wa.141855a9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mycv/precache-manifest.337ff6bbac13adf9d230e5024d0dd6bf.js"
+  "/mycv/precache-manifest.0a798b18ee37c7a92a7bb2be393cfa16.js"
 );
 
 self.addEventListener('message', (event) => {
