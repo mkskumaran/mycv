6e6aa921cf9eb616fe8b053dd8c1a5e7dbe6934a
ShunmugaKumaran M <34643845+mkskumaran@users.noreply.github.com>
2020-09-28 19:18:40 +0530

    Updates

--- precache-manifest.5a6a50728b7d9c3a3bb7798f0b3e1c29.js
+++ precache-manifest.5a6a50728b7d9c3a3bb7798f0b3e1c29.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ec2b19c61103e473e1ea98eacebba5b",
+    "revision": "8146208f2dcfe4134a2bff331a3a9fef",
     "url": "/mycv/index.html"
   },
   {
-    "revision": "fa8f800f2c9546065f68",
+    "revision": "5fd38abba8148b0effc9",
     "url": "/mycv/static/css/2.a286b187.chunk.css"
   },
   {
-    "revision": "c2a5e01ae4f1323f9bbf",
+    "revision": "06ea3624d132c4186c6d",
     "url": "/mycv/static/css/main.fa4eb233.chunk.css"
   },
   {
-    "revision": "fa8f800f2c9546065f68",
-    "url": "/mycv/static/js/2.d9ba7e7a.chunk.js"
+    "revision": "5fd38abba8148b0effc9",
+    "url": "/mycv/static/js/2.d540b46a.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/mycv/static/js/2.d9ba7e7a.chunk.js.LICENSE.txt"
+    "url": "/mycv/static/js/2.d540b46a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c2a5e01ae4f1323f9bbf",
-    "url": "/mycv/static/js/main.b8db7bd0.chunk.js"
+    "revision": "06ea3624d132c4186c6d",
+    "url": "/mycv/static/js/main.307b05fe.chunk.js"
   },
   {
     "revision": "9c9156fde34d30785013",
@@ -46,5 +46,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/mycv/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "59e851787e21900cc0e494e17d7b60ea",
+    "url": "/mycv/static/media/me.59e85178.png"
   }
 ]);
